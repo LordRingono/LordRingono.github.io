79a3870aa9883a5d80d269fdf0827fdbe8c0a82b
raphi35132 <11164956-raphi35132@users.noreply.replit.com>
2023-07-29 17:37:52 +0000

    added a few functionalities

--- cookies.js
+++ cookies.js
@@ -1,17 +1,21 @@
 var cookies = 0
 var four = 1
+var usines = 1
+var cookies_total = 0
 function cookiesFunc() {
   cookies = cookies + four;
+  cookies_total += four;
   let paragraphe = "Vous avez " + cookies.toString() + " cookies";
-  document.getElementById("nbcookies").innerHTML = paragraphe;
+document.getElementById("nbcookies").innerHTML = paragraphe;
+  let paragraphe4 = "Vous avez produit " + cookies_total.toString() + " cookies depuis le début de l'aventure";
+document.getElementById("compteur").innerHTML = paragraphe4;
 }
 function cuire() {
   if (cookies >= 25) {
     cookies = cookies - 25;
-    four = four + 1;
-    console.log("hey")
+    four = four + usines;
     let paragraphe = "Vous avez " + cookies.toString() + " cookies";
-    document.getElementById("nbcookies").innerHTML = paragraphe;
+document.getElementById("nbcookies").innerHTML = paragraphe;
     console.log("hey2")
     let paragraphe2 = "Vous avez " + four.toString() + "      fours";
     document.getElementById("fours").innerHTML = paragraphe2;
@@ -20,5 +24,16 @@ function cuire() {
     alert("Tu n'a pas assé de cookies pour acheter un nouveau four");
   }
 }
-
-
+function construire() {
+  if (cookies >= 500) {
+    cookies -= 500;
+    usines += 1;
+    let paragraphe = "Vous avez " + cookies.toString() + " cookies";
+    document.getElementById("nbcookies").innerHTML = paragraphe;
+    let paragraphe3 = "Vous avez " + usines.toString() + " usines";
+    document.getElementById("usines").innerHTML = paragraphe3;
+  }
+  else {
+    alert("Tu n'a pas assé de cookies pour construire une nouvelle usine");
+  }
+}
